ecbb77384b230c232847f549031a6abea930ef1e
cgsg185 <goshik200712@gmail.com>
2023-06-20 19:46:09 +0300

    Almost finished BATTLESHIP - bug fixes + dead enemies required

--- 1906/client/main.js
+++ 1906/client/main.js
@@ -1,51 +1,320 @@
 import { io } from "socket.io-client";
 
-let inMM = false;
+let inMM = false,
+  inGame = false,
+  deploying = false,
+  attacking = false;
+
+let myboard = document.getElementById("myBoardTable");
+let enemyboard = document.getElementById("enemyBoardTable");
+
+let myboarddata = myboard.querySelectorAll("td");
+let enemyboarddata = enemyboard.querySelectorAll("td");
+
+window.addEventListener("mousedown", () => {
+  mpress = true;
+});
+
+let board1, board2;
+
+function boardsColorsSet() {
+  let i, j;
+
+  for (i = 0; i < 11; i++)
+    for (j = 0; j < 11; j++) {
+      if (i == 0 || j == 0)
+        myboarddata[i * 11 + j].style.backgroundColor = "rgb(60, 100, 100)";
+      else {
+        if (inGame && board1 != undefined) {
+          switch (board1[(i - 1) * 10 + (j - 1)]) {
+            case "0":
+              myboarddata[i * 11 + j].style.backgroundColor = "blue";
+              myboarddata[i * 11 + j].style.innerText = "";
+              myboarddata[i * 11 + j].style.color = "black";
+              break;
+            case "2":
+              myboarddata[i * 11 + j].style.backgroundColor = "black";
+              myboarddata[i * 11 + j].style.innerText = "";
+              myboarddata[i * 11 + j].style.color = "black";
+              break;
+            case "3":
+              myboarddata[i * 11 + j].style.backgroundColor = "black";
+              myboarddata[i * 11 + j].style.innerText = "X";
+              myboarddata[i * 11 + j].style.color = "white";
+              break;
+            case "4":
+              myboarddata[i * 11 + j].style.backgroundColor = "blue";
+              myboarddata[i * 11 + j].style.innerText = "X";
+              myboarddata[i * 11 + j].style.color = "black";
+              break;
+            case "5":
+              myboarddata[i * 11 + j].style.backgroundColor = "white";
+              myboarddata[i * 11 + j].style.innerText = "X";
+              myboarddata[i * 11 + j].style.color = "black";
+              break;
+          }
+          if (deploying)
+            if (j == x + 1 && i == y + 1) {
+              myboarddata[i * 11 + j].innerText = "X";
+              myboarddata[i * 11 + j].color = "black";
+            }
+        } else {
+          myboarddata[i * 11 + j].style.backgroundColor = "aliceblue";
+          myboarddata[i * 11 + j].style.color = "black";
+        }
+      }
+    }
+
+  for (i = 0; i < 11; i++)
+    for (j = 0; j < 11; j++) {
+      if (i == 0 || j == 0)
+        enemyboarddata[i * 11 + j].style.backgroundColor = "rgb(60, 100, 100)";
+      else {
+        if (inGame && board2 != undefined) {
+          switch (board2[(i - 1) * 10 + (j - 1)]) {
+            case "0":
+              enemyboarddata[i * 11 + j].style.backgroundColor = "blue";
+              enemyboarddata[i * 11 + j].innerText = "";
+              enemyboarddata[i * 11 + j].style.color = "black";
+              break;
+            case "1":
+              enemyboarddata[i * 11 + j].style.backgroundColor = "grey";
+              enemyboarddata[i * 11 + j].innerText = "";
+              enemyboarddata[i * 11 + j].style.color = "black";
+              break;
+            case "2":
+              enemyboarddata[i * 11 + j].style.backgroundColor = "black";
+              enemyboarddata[i * 11 + j].innerText = "";
+              enemyboarddata[i * 11 + j].style.color = "black";
+              break;
+            case "3":
+              enemyboarddata[i * 11 + j].style.backgroundColor = "black";
+              enemyboarddata[i * 11 + j].style.innerText = "X";
+              enemyboarddata[i * 11 + j].style.color = "white";
+              break;
+            case "4":
+              enemyboarddata[i * 11 + j].style.backgroundColor = "blue";
+              enemyboarddata[i * 11 + j].style.innerText = "X";
+              enemyboarddata[i * 11 + j].style.color = "black";
+              break;
+            case "5":
+              enemyboarddata[i * 11 + j].style.backgroundColor = "white";
+              enemyboarddata[i * 11 + j].style.innerText = "X";
+              enemyboarddata[i * 11 + j].style.color = "black";
+              break;
+          }
+          if (attacking)
+            if (j == xe + 1 && i == ye + 1) {
+              enemyboarddata[i * 11 + j].innerText = "X";
+              enemyboarddata[i * 11 + j].style.color = "red";
+            }
+        } else {
+          enemyboarddata[i * 11 + j].style.backgroundColor = "aliceblue";
+          enemyboarddata[i * 11 + j].style.color = "black";
+        }
+      }
+    }
+}
+
+let x = -1,
+  y = -1;
+let xe = -1,
+  ye = -1;
+
+let mpress = false;
+
+myboard.addEventListener("mousemove", (event) => {
+  let x0 = myboard.offsetLeft;
+  x = Math.floor(((event.pageX - x0) / myboard.clientWidth) * 11) - 1;
+  let y0 = myboard.offsetTop;
+  y = Math.floor(((event.pageY - y0) / myboard.clientHeight) * 11) - 1;
+
+  boardsColorsSet();
+  event.preventDefault();
+});
+
+enemyboard.addEventListener("mousemove", (event) => {
+  let x0 = enemyboard.offsetLeft;
+  xe = Math.floor(((event.pageX - x0) / enemyboard.clientWidth) * 11) - 1;
+  let y0 = enemyboard.offsetTop;
+  ye = Math.floor(((event.pageY - y0) / enemyboard.clientHeight) * 11) - 1;
+
+  boardsColorsSet();
+  event.preventDefault();
+});
 
 async function main() {
-  const socket = io();
+  let socket = io();
 
-  // client-side
+  boardsColorsSet();
+  // connected to server
   socket.on("connect", () => {
     console.log(`Connected to server with id ${socket.id}`);
+    document.getElementById("myBoardName").innerHTML = `${socket.id} (You)`;
   });
 
-  document
-    .getElementById("matchmakeButton")
-    .addEventListener("mousedown", () => {
+  // button press
+  document.getElementById("theButton").addEventListener("mousedown", () => {
+    if (!inGame) {
       if (!inMM) socket.emit("matchmakeRequest");
       else socket.emit("leaveMMrequest");
-    });
+    } else socket.emit("concedeRequest");
+  });
 
+  // matchmaking request confirms
   socket.on("joinedMM", () => {
-    document.getElementById("matchmakePrompt").innerHTML =
+    document.getElementById("thePrompt").style.color = "black";
+    document.getElementById("thePrompt").innerHTML =
       "You have joined the matchmaking queue";
-    document.getElementById("matchmakeButton").value =
-      "Leave queue"; 
+    document.getElementById("theButton").value = "Leave queue";
     inMM = true;
   });
   socket.on("alreadyInMM", () => {
-    document.getElementById("matchmakePrompt").innerHTML =
+    document.getElementById("thePrompt").style.color = "black";
+    document.getElementById("thePrompt").innerHTML =
       "You are already in matchmaking!";
   });
-  socket.on("leftMM", ()=>{
-    document.getElementById("matchmakePrompt").innerHTML =
+  socket.on("leftMM", () => {
+    document.getElementById("thePrompt").style.color = "black";
+    document.getElementById("thePrompt").innerHTML =
       "You have left the matchmaking queue";
-    document.getElementById("matchmakeButton").value =
-      "Join queue"; 
+    document.getElementById("theButton").value = "Join queue";
     inMM = false;
   });
 
+  // players in MM counter update
   socket.on("updateMMplayerCounter", (count) => {
+    if (!inGame)
+      document.getElementById(
+        "matchmakePlayerCount"
+      ).innerHTML = `There are currently ${count} players in matchmaking queue`;
+    else document.getElementById("matchmakePlayerCount").innerHTML = "";
+  });
+
+  // game
+  socket.on("gameStart", (enemyID) => {
+    document.getElementById("thePrompt").style.color = "black";
+    document.getElementById("thePrompt").innerHTML = "You joined the game!";
+    document.getElementById("enemyBoardName").innerHTML = `${enemyID} (Enemy)`;
+    document.getElementById("theButton").value = "Concede";
+    (inMM = false), (inGame = true), (mpress = false);
+    socket.emit("gameDeploy", -1, -1);
+  });
+  socket.on("gameEnd", (status) => {
+    document.getElementById("gamePrompt").innerHTML =
+      status == "win"
+        ? "You win!"
+        : status == "lose"
+        ? "You lost!"
+        : "Both players didn't prepare their boards in time! You both suck!";
+    document.getElementById("enemyBoardName").innerHTML =
+      "No one's here... yet... (Enemy)";
+    document.getElementById("theButton").value = "Join queue";
+    inGame = false;
+    //boardsColorsSet();
+  });
+  socket.on("concedeSuccess", () => {
+    document.getElementById("thePrompt").style.color = "black";
     document.getElementById(
-      "matchmakePlayerCount"
-    ).innerHTML = `There are currently ${count} players in matchmaking queue`;
+      "thePrompt"
+    ).innerHTML = `You have conceded and left the game`;
   });
-  /*
-  socket.on("disconnect", () => {
-    console.log(`${socket.id} (you) has disconnected`);
+  socket.on("opponentConcede", () => {
+    document.getElementById("thePrompt").style.color = "black";
+    document.getElementById("thePrompt").innerHTML = `${
+      document.getElementById("enemyBoardName").innerHTML
+    } admits their weakness and concedes!`;
   });
-  */
+  socket.on("opponentLeft", () => {
+    document.getElementById("thePrompt").style.color = "black";
+    document.getElementById("thePrompt").innerHTML = `${
+      document.getElementById("enemyBoardName").innerHTML
+    } left the game`;
+  });
+  socket.on("gameUpdate", (num, phase, boardMine, boardOpp) => {
+    switch (phase) {
+      case "Setup":
+        document.getElementById(
+          "gamePrompt"
+        ).innerHTML = `Setup phase! Deploy your ships! ${num} seconds left!`;
+        deploying = true;
+        attacking = false;
+        if (mpress) socket.emit("gameDeploy", x, y);
+        break;
+      case "Battle":
+        document.getElementById("gamePrompt").innerHTML =
+          "Battle phase! Attack the enemy! ";
+        document.getElementById("gamePrompt").innerHTML +=
+          num == 1 ? "Your turn!" : "The enemie's turn!";
+        deploying = false;
+        attacking = num == 1 ? true : false;
+        if (mpress) socket.emit("gameAttack", xe, ye);
+        break;
+    }
+    board1 = boardMine;
+    board2 = boardOpp;
+    boardsColorsSet();
+    mpress = false;
+  });
+  socket.on("boardVerification", (status) => {
+    switch (status) {
+      case "allGood":
+        document.getElementById("thePrompt").style.color = "black";
+        document.getElementById("thePrompt").innerHTML =
+          "Board verified! You're all set!";
+        break;
+      case "cornersWrong":
+        document.getElementById("thePrompt").style.color = "red";
+        document.getElementById("thePrompt").innerHTML =
+          "Ships can't be touching corners with each other!";
+        break;
+      case "shipTooLong":
+        document.getElementById("thePrompt").style.color = "red";
+        document.getElementById("thePrompt").innerHTML =
+          "You can't have ships longer than 4 cells!";
+        break;
+      case "4cellWrong":
+        document.getElementById("thePrompt").style.color = "red";
+        document.getElementById("thePrompt").innerHTML =
+          "You should have ONE 4-long ship!";
+        break;
+      case "3cellWrong":
+        document.getElementById("thePrompt").style.color = "red";
+        document.getElementById("thePrompt").innerHTML =
+          "You should have TWO 3-long ships!";
+        break;
+      case "2cellWrong":
+        document.getElementById("thePrompt").style.color = "red";
+        document.getElementById("thePrompt").innerHTML =
+          "You should have THREE 2-long ships!";
+        break;
+      case "1cellWrong":
+        document.getElementById("thePrompt").style.color = "red";
+        document.getElementById("thePrompt").innerHTML =
+          "You should have FOUR 1-long ships!";
+        break;
+    }
+  });
+  socket.on("shotFeedback", (id, shotStatus) => {
+    switch (shotStatus) {
+      case "miss":
+        document.getElementById("thePrompt").style.color = "black";
+        document.getElementById("thePrompt").innerHTML = `${id} missed!`;
+        break;
+      case "hit":
+        document.getElementById("thePrompt").style.color = "black";
+        document.getElementById(
+          "thePrompt"
+        ).innerHTML = `${id} hit! ${id} can attack again!`;
+        break;
+      case "shotThereAlready":
+        document.getElementById("thePrompt").style.color = "black";
+        document.getElementById(
+          "thePrompt"
+        ).innerHTML = `${id} shot a cell that was already shot and can attack again.`;
+        break;
+    }
+  });
 }
 
 window.addEventListener("load", (event) => {
--- 1906/server/server.js
+++ 1906/server/server.js
@@ -2,7 +2,6 @@ const http = require("http");
 const express = require("express");
 const morgan = require("morgan");
 const { Server } = require("socket.io");
-const { match } = require("assert");
 
 const app = express();
 app.use(morgan("combined"));
@@ -12,18 +11,363 @@ app.use(express.static("."));
 const server = http.createServer(app);
 const io = new Server(server);
 
-setInterval(() => {
-  io.emit("updateMMplayerCounter", matchmaking.length);
-}, 500);
+function getTime() {
+  const date = new Date();
+  return (
+    date.getMilliseconds() / 1000.0 +
+    date.getSeconds() +
+    date.getMinutes() * 60.0
+  );
+}
 
 const clients = [];
 const matchmaking = [];
 const gamelobbies = [];
 
+setInterval(() => {
+  io.emit("updateMMplayerCounter", matchmaking.length);
+}, 500);
+
+setInterval(() => {
+  for (let lobby of gamelobbies) lobby.gameStep();
+}, 100);
+
+function destroyLobby(lobby) {
+  let index = gamelobbies.indexOf(lobby);
+  if (index > -1) gamelobbies.splice(index, 1);
+}
+
+class gameboard {
+  constructor() {
+    this.a = [];
+    for (let i = 0; i < 10; i++) this.a[i] = [];
+
+    for (let i = 0; i < 10; i++)
+      for (let j = 0; j < 10; j++) this.a[i][j] = "sea";
+  }
+  deploy(x, y) {
+    if (x < 0 || x > 9 || y < 0 || y > 9) return;
+    this.a[y][x] = this.a[y][x] == "sea" ? "ship" : "sea";
+  }
+  boardOut(conceal) {
+    let res = "";
+
+    for (let i = 0; i < 10; i++)
+      for (let j = 0; j < 10; j++) {
+        if (conceal) {
+          switch (this.a[i][j]) {
+            case "sea":
+            case "ship":
+              res += "1";
+              break;
+            case "hit":
+              res += "3";
+              break;
+            case "miss":
+              res += "4";
+              break;
+            case "dead":
+              res += "5";
+              break;
+          }
+        } else {
+          switch (this.a[i][j]) {
+            case "sea":
+              res += "0";
+              break;
+            case "ship":
+              res += "2";
+              break;
+            case "hit":
+              res += "3";
+              break;
+            case "miss":
+              res += "4";
+              break;
+            case "dead":
+              res += "5";
+              break;
+          }
+        }
+      }
+
+    // console.log(res);
+    return res;
+  }
+  checkCorners() {
+    for (let i = 0; i < 10; i++)
+      for (let j = 0; j < 10; j++) {
+        if (this.a[i][j] == "ship")
+          if (
+            (i > 0 && j > 0 && this.a[i - 1][j - 1] == "ship") ||
+            (i > 0 && j < 9 && this.a[i - 1][j + 1] == "ship") ||
+            (i < 9 && j > 0 && this.a[i + 1][j - 1] == "ship") ||
+            (i < 9 && j < 9 && this.a[i + 1][j + 1] == "ship")
+          )
+            return false;
+      }
+
+    return true;
+  }
+  fullValidate() {
+    if (this.checkCorners()) {
+      // ship counters
+      let ships = [];
+      ships[0] = 0;
+      ships[1] = 0;
+      ships[2] = 0;
+      ships[3] = 0;
+
+      // array for cells with ships that were counted
+      let checked = [];
+      for (let i = 0; i < 10; i++)
+        for (let j = 0; j < 10; j++) checked[i * 10 + j] = false;
+
+      for (let i = 0; i < 10; i++)
+        for (let j = 0; j < 10; j++) {
+          if (this.a[i][j] != "ship" || checked[i * 10 + j]) continue;
+          else {
+            // there's a new ship
+            checked[i * 10 + j] = true;
+            let k;
+            // see if it's horizontal
+            for (k = 1; k != 5; k++) {
+              if (j + k < 10 && this.a[i][j + k] == "ship")
+                checked[i * 10 + j + k] = true;
+              else break;
+            }
+            if (k == 5) return "shipTooLong";
+            if (k > 1) {
+              ships[k - 1]++;
+              continue;
+            }
+            // see if it's vertical
+            for (k = 1; k != 5; k++) {
+              if (i + k < 10 && this.a[i + k][j] == "ship")
+                checked[(i + k) * 10 + j] = true;
+              else break;
+            }
+            if (k == 5) return "shipTooLong";
+            if (k > 1) {
+              ships[k - 1]++;
+              continue;
+            }
+            ships[0]++;
+          }
+        }
+
+      if (ships[3] != 1) return "4cellWrong";
+      if (ships[2] != 2) return "3cellWrong";
+      if (ships[1] != 3) return "2cellWrong";
+      if (ships[0] != 4) return "1cellWrong";
+
+      return "allGood";
+    } else return "cornersWrong";
+  }
+  shootAt(x, y) {
+    if (x < 0 || x > 9 || y < 0 || y > 9) return;
+    if (this.a[y][x] == "ship") {
+      this.a[y][x] = "hit";
+      return "hit";
+    } else if (this.a[y][x] == "sea") {
+      this.a[y][x] = "miss";
+      return "miss";
+    } else if (this.a[y][x] == "miss" || this.a[y][x] == "hit") {
+      return "shotThereAlready";
+    }
+  }
+  isAlive() {
+    for (let i = 0; i < 10; i++)
+      for (let j = 0; j < 10; j++) if (this.a[i][j] == "ship") return true;
+
+    return false;
+  }
+}
+
 class gamelobby {
+  gameEnd(winningPlayer) {
+    // return default callbacks
+    this.P1.on("disconnect", () => {
+      console.log(`Client with id ${this.P1.id} has disconnected`);
+      let index = clients.indexOf(this.P1);
+      if (index > -1) {
+        clients.splice(index, 1);
+        console.log(`${this.P1.id} removed from client list`);
+      }
+      index = matchmaking.indexOf(this.P1);
+      if (index > -1) {
+        matchmaking.splice(index, 1);
+        console.log(`${this.P1.id} left matchmaking due to disconnection`);
+      }
+    });
+    this.P2.on("disconnect", () => {
+      console.log(`Client with id ${this.P2.id} has disconnected`);
+      let index = clients.indexOf(this.P2);
+      if (index > -1) {
+        clients.splice(index, 1);
+        console.log(`${this.P2.id} removed from client list`);
+      }
+      index = matchmaking.indexOf(this.P2);
+      if (index > -1) {
+        matchmaking.splice(index, 1);
+        console.log(`${this.P2.id} left matchmaking due to disconnection`);
+      }
+    });
+
+    // emit game end signal
+    if (winningPlayer == 1) {
+      this.P1.emit("gameEnd", "win");
+      this.P2.emit("gameEnd", "lose");
+    } else if (winningPlayer == 2) {
+      this.P1.emit("gameEnd", "lose");
+      this.P2.emit("gameEnd", "win");
+    } else {
+      this.P1.emit("gameEnd", "draw");
+      this.P2.emit("gameEnd", "draw");
+    }
+
+    // destroy the lobby
+    destroyLobby(this);
+  }
   constructor(socket1, socket2) {
+    // store player sockets
     this.P1 = socket1;
     this.P2 = socket2;
+    // assign a board to each player
+    this.board1 = new gameboard();
+    this.board2 = new gameboard();
+    // set first attacker
+    this.attacker = 1;
+    // assign new P1 client callbacks
+    this.P1.on("disconnect", () => {
+      console.log(`Client with id ${this.P1.id} has disconnected`);
+      let index = clients.indexOf(this.P1);
+      if (index > -1) {
+        clients.splice(index, 1);
+        console.log(`${this.P1.id} removed from client list`);
+      }
+      this.P2.emit("opponentLeft");
+      this.gameEnd(2);
+    });
+    this.P1.on("concedeRequest", () => {
+      console.log(`Client with id ${this.P1.id} has left the game`);
+      this.P1.emit("concedeSuccess");
+      this.P2.emit("opponentConcede");
+      this.gameEnd(2);
+    });
+    this.P1.on("gameDeploy", (x, y) => {
+      if (this.phase != "Setup") return;
+      this.board1.deploy(x, y);
+      this.P1.emit("boardVerification", this.board1.fullValidate());
+    });
+    this.P1.on("gameAttack", (x, y) => {
+      if (this.phase != "Battle") return;
+      if (this.attacker == 1) {
+        let shotStatus = this.board2.shootAt(x, y);
+        if (shotStatus == "miss") this.attacker = 2;
+        this.P1.emit("shotFeedback", this.P2.id, shotStatus);
+        this.P2.emit("shotFeedback", this.P2.id, shotStatus);
+      }
+    });
+
+    // assign new P2 client callbacks
+    this.P2.on("disconnect", () => {
+      console.log(`Client with id ${this.P2.id} has disconnected`);
+      let index = clients.indexOf(this.P2);
+      if (index > -1) {
+        clients.splice(index, 1);
+        console.log(`${this.P2.id} removed from client list`);
+      }
+      this.P1.emit("opponentLeft");
+      this.gameEnd(1);
+    });
+    this.P2.on("concedeRequest", () => {
+      console.log(`Client with id ${this.P2.id} has left the game`);
+      this.P2.emit("concedeSuccess");
+      this.P1.emit("opponentConcede");
+      this.gameEnd(1);
+    });
+    this.P2.on("gameDeploy", (x, y) => {
+      if (this.phase != "Setup") return;
+      this.board2.deploy(x, y);
+      this.P2.emit("boardVerification", this.board2.fullValidate());
+    });
+    this.P2.on("gameAttack", (x, y) => {
+      if (this.phase != "Battle") return;
+      if (this.attacker == 2) {
+        let shotStatus = this.board1.shootAt(x, y);
+        if (shotStatus == "miss") this.attacker = 1;
+        this.P1.emit("shotFeedback", this.P2.id, shotStatus);
+        this.P2.emit("shotFeedback", this.P2.id, shotStatus);
+      }
+    });
+  }
+  gameStart() {
+    // emit game start signal
+    this.P1.emit("gameStart", this.P2.id);
+    this.P2.emit("gameStart", this.P1.id);
+
+    // start setup phase
+    this.setphstTime = getTime();
+    this.phase = "Setup";
+
+    // emit setup phase start signal
+    // this.P1.emit("gameSetupPhaseStart");
+    // this.P2.emit("gameSetupPhaseStart");
+  }
+  gameStep() {
+    switch (this.phase) {
+      case "Setup":
+        let setupTime = 30;
+        this.P1.emit(
+          "gameUpdate",
+          Math.floor(setupTime - (getTime() - this.setphstTime)),
+          this.phase,
+          this.board1.boardOut(false),
+          this.board2.boardOut(true)
+        );
+        this.P2.emit(
+          "gameUpdate",
+          Math.floor(setupTime - (getTime() - this.setphstTime)),
+          this.phase,
+          this.board2.boardOut(false),
+          this.board1.boardOut(true)
+        );
+        if (setupTime - (getTime() - this.setphstTime) <= 0) {
+          if (
+            this.board1.fullValidate() == "allGood" &&
+            this.board2.fullValidate() == "allGood"
+          )
+            this.phase = "Battle";
+          else if (
+            this.board1.fullValidate() != "allGood" &&
+            this.board2.fullValidate() != "allGood"
+          )
+            this.gameEnd(0);
+          else if (this.board1.fullValidate() != "allGood") this.gameEnd(2);
+          else this.gameEnd(1);
+        }
+        break;
+      case "Battle":
+        this.P1.emit(
+          "gameUpdate",
+          this.attacker == 1 ? 1 : 2,
+          this.phase,
+          this.board1.boardOut(false),
+          this.board2.boardOut(true)
+        );
+        this.P2.emit(
+          "gameUpdate",
+          this.attacker == 2 ? 1 : 2,
+          this.phase,
+          this.board2.boardOut(false),
+          this.board1.boardOut(true)
+        );
+        if (!this.board1.isAlive() && !this.board2.isAlive()) this.gameEnd(0);
+        else if (!this.board1.isAlive()) this.gameEnd(2);
+        else if (!this.board2.isAlive()) this.gameEnd(1);
+        break;
+    }
   }
 }
 
@@ -52,8 +396,22 @@ io.on("connection", (socket) => {
 
     // start a match if there are at least 2 clients in matchmaking queue
     if (matchmaking.length >= 2) {
+      // pick player 1 and remove from matchmaking
       let randI = Math.floor(Math.random() * matchmaking.length);
-      
+      const P1 = matchmaking[randI];
+      matchmaking.splice(randI, 1);
+      console.log(`${socket.id} left matchmaking and entered a lobby`);
+
+      // pick player 2 and remove from matchmaking
+      randI = Math.floor(Math.random() * matchmaking.length);
+      const P2 = matchmaking[randI];
+      matchmaking.splice(randI, 1);
+      console.log(`${socket.id} left matchmaking and entered a lobby`);
+
+      // create new lobby and start the game
+      const newlobby = new gamelobby(P1, P2);
+      newlobby.gameStart();
+      gamelobbies.push(newlobby);
     }
   });
   socket.on("leaveMMrequest", () => {
